2836061576441a0eaa9982a281b7cea7943706ee
dera-nnamani <158452490+dera-nnamani@users.noreply.github.com>
2024-04-16 13:41:37 +0100

    Add files via upload

---
+++ sorting.js
@@ -0,0 +1,21 @@
+function insertionSort(arr) {
+  const n = arr.length;
+  for (let i = 1; i < n; i++) {
+    let key = arr[i];
+    let j = i - 1;
+
+    // Move elements of arr[0..i-1], that are greater than key, to one position ahead of their current position
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+
+// Example usage:
+const array = [12, 11, 13, 5, 6];
+console.log("Original Array:", array);
+const sortedArray = insertionSort(array);
+console.log("Sorted Array:", sortedArray);
